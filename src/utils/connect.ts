f09fe70a40ef91ce8558a895fa284169fb17c6c2
Moso <mostafa@DESKTOP-0AHVU0S.localdomain>
2022-05-15 21:19:32 -0400

    compose

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,18 +1,19 @@
-import { connect } from "http2";
+import {connect} from "http2";
 import mongoose from "mongoose";
-import { config } from "dotenv";
+import {config} from "dotenv";
 import logger from "./logger";
 export default async () => {
-  const envFile = process.env.NODE_ENV
-    ? `.env.${process.env.NODE_END}`
-    : ".env";
-  config({ path: envFile });
-  const dbUri: string = process.env.URI || "mongodb://localhost:27017/test";
-  try {
-    await mongoose.connect(dbUri);
-    logger.info("Connected to database...");
-  } catch (e) {
-    logger.fatal("Database connection failed. Exiting...");
-    process.exit(1);
-  }
+    const envFile = process.env.NODE_ENV
+        ? `.env.${process.env.NODE_END}`
+        : ".env";
+    config({path: envFile});
+    const dbUri: string = process.env.URI || "mongodb://mongodb:27017/test";
+    logger.info(`dburi: ${dbUri} `);
+    try {
+        await mongoose.connect(dbUri);
+        logger.info("Connected to database...");
+    } catch (e) {
+        logger.fatal("Database connection failed. Exiting...");
+        process.exit(1);
+    }
 };
