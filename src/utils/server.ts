26359be95391830f9da14d57fef092434a0e07d8
mostafa-wahib <mostafa.wahib@outlook.com>
2022-06-12 14:26:29 -0400

    added ratelimiting

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -4,12 +4,20 @@ import cors from "cors";
 import { deserializeUser } from "../middleware/deserializeUser";
 import routes from "../routes";
 import swaggerDocs from "../utils/swagger";
+import rateLimit from "express-rate-limit";
 export default function () {
   const envFile = process.env.NODE_ENV
     ? `.env.${process.env.NODE_ENV}`
     : ".env";
   config({ path: envFile });
+  const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 150, // Limit each IP to 150 requests per `window` (here, per 10 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  });
   const app = express();
+  app.use(limiter);
   app.use(cors());
   app.use(express.json());
   app.use(deserializeUser);
